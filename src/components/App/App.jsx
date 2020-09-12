2de8542ab40ab3fdd8a07a7c87ac7362a1e21bec
Marcelaomp <marcelaompereira@gmail.com>
2020-09-11 21:44:38 -0300

    add app component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 // import './App.css'
-import {Wrapper} from './App.styles'
+import {Wrapper, Container} from './App.styles'
 
 function App () {
     return <Wrapper>
-        todo:app
+        <Container>
+            todo:app
+            </Container>
     </Wrapper>
 }
 
